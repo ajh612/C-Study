b1ed5181a28f1756206726dc66f31dcc8a8d8a67
ajh612 <111953785+ajh612@users.noreply.github.com>
2022-09-13 09:46:51 +0900

    study

--- problem_10809.cpp
+++ problem_10809.cpp
@@ -1,13 +1,32 @@
+//영어 소문자는 26개
 #include <stdio.h>
 #include <string.h>
 
-int number[100] = {0};
-
 int main(){
-	char num;
-	                     //26 
-	scanf("%s", &number);
-	for (int i=0; i<26; i++){
-		if( )
+	 
+	// 문자열을 만든다
+	char a[100]={0};
+	
+	//문자열을 입력받는다
+	scanf ("%s", a);
+	
+	//문자열을 한 글자 씩 검사한다.
+	int length = strlen(a);
+	int sw=0;
+	for	(int i=97; i<123; i++){
+		
+		for (int j=0; j<length; j++){
+			if( i == a[j]){
+			printf("%d ", j);
+			sw=1;
+			break;
+			}
+		}
+		
+		if (sw==0) printf("-1 ");
+		
+		sw=0;
 	}
+	return 0;
 }
+
